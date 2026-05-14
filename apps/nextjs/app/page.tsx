013d51fb97ec277559216764351335954bd9c2de
tooj <tjnyaina@gmail.com>
2026-05-14 14:10:49 +0300

    add - nextJS

---
+++ apps/nextjs/app/page.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function Home() {
+  return (
+    <div className="flex flex-1 font-sans">
+      <Sidebar/>
+    </div>
+  );
+}
---
+++ apps/nextjs/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className="flex min-h-screen">
+      <aside className="flex w-64 border-r border-border bg-card/50">
+
+      </aside>
+    </div>
+  );
+};
+
+export default Sidebar;
