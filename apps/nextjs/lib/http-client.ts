4a82c98cd4f3a8844b808864bef26ae8252ca943
tooj <tjnyaina@gmail.com>
2026-05-15 14:32:36 +0300

    patch(apps) - next: httpClient (handleResponse)

--- apps/nextjs/lib/http-client.ts
+++ apps/nextjs/lib/http-client.ts
@@ -2,32 +2,45 @@ const headers = {
   'x-typesense-api-key': 'xyz',
 }
 
+const handleResponse = async (res: Response) => {
+  const data = await res.json().catch(() => null);
+
+  if (!res.ok) {
+    throw new Error(data?.message ?? res.statusText);
+  }
+
+  return data;
+};
+
 export const httpClient = {
   get: async (url: string) => {
     const res = await fetch(url, { headers });
-    return res.json();
+    return handleResponse(res);
   },
+
   post: async (url: string, data: any) => {
     const res = await fetch(url, {
       method: 'POST',
       body: JSON.stringify(data),
-      headers
+      headers,
     });
-    return res.json();
+    return handleResponse(res);
   },
+
   put: async (url: string, data: any) => {
     const res = await fetch(url, {
       method: 'PUT',
       body: JSON.stringify(data),
-      headers
+      headers,
     });
-    return res.json();
+    return handleResponse(res);
   },
+
   delete: async (url: string) => {
     const res = await fetch(url, {
       method: 'DELETE',
-      headers
+      headers,
     });
-    return res.json();
+    return handleResponse(res);
   },
 };
